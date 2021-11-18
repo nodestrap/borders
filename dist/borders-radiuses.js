d0ce7e2354f4d158f3f3bc177a0905ed74ccfcfb
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-19 02:17:40 +0700

    converted to es6 module

--- dist/borders-radiuses.js
+++ dist/borders-radiuses.js
@@ -1,13 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = exports.radiuses = exports.cssConfig = exports.cssVals = exports.cssDecls = exports.cssProps = void 0;
-const css_config_1 = __importDefault(require("@cssfn/css-config")); // Stores & retrieves configuration using *css custom properties* (css variables)
+import createCssConfig from '@cssfn/css-config'; // Stores & retrieves configuration using *css custom properties* (css variables)
 // configs:
-_a = (0, css_config_1.default)(() => {
+export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
     const basics = {
         none: '0px',
         sm: '0.2rem',
@@ -23,6 +16,5 @@ _a = (0, css_config_1.default)(() => {
         ...basics,
         ...defaults,
     };
-}, { prefix: 'bd-rd' }), exports.default = exports.radiuses = exports.cssProps = _a[0], exports.cssDecls = _a[1], exports.cssVals = _a[2], exports.cssConfig = _a[3];
-exports.radiuses = exports.cssProps;
-exports.default = exports.cssProps;
+}, { prefix: 'bd-rd' });
+export { cssProps as radiuses, cssProps as default };
--- dist/borders.js
+++ dist/borders.js
@@ -1,39 +1,11 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = exports.borders = exports.cssConfig = exports.cssVals = exports.cssDecls = exports.cssProps = exports.borderRadius = exports.borderRadiuses = exports.radius = exports.radiuses = void 0;
-const css_config_1 = __importDefault(require("@cssfn/css-config")); // Stores & retrieves configuration using *css custom properties* (css variables)
+import createCssConfig from '@cssfn/css-config'; // Stores & retrieves configuration using *css custom properties* (css variables)
 // nodestrap utilities:
-const radius = __importStar(require("./borders-radiuses"));
-exports.radius = radius;
-exports.borderRadius = radius;
+import * as radius from './borders-radiuses';
 const radiuses = radius.radiuses;
-exports.radiuses = radiuses;
-exports.borderRadiuses = radiuses;
+export { radiuses, radius };
+export { radiuses as borderRadiuses, radius as borderRadius };
 // configs:
-_a = (0, css_config_1.default)(() => {
+export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
     const widths = {
         none: '0px',
         hair: '1px',
@@ -53,6 +25,5 @@ _a = (0, css_config_1.default)(() => {
         ...styles,
         ...defaults,
     };
-}, { prefix: 'bd' }), exports.default = exports.borders = exports.cssProps = _a[0], exports.cssDecls = _a[1], exports.cssVals = _a[2], exports.cssConfig = _a[3];
-exports.borders = exports.cssProps;
-exports.default = exports.cssProps;
+}, { prefix: 'bd' });
+export { cssProps as borders, cssProps as default };
